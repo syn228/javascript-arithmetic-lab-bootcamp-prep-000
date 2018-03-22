b7ee2dc97abbea04761776fcf76957132397d5b7
Aaron Na <syn228@nyu.edu>
2018-03-22 17:42:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function add(a,b){
 }
 function subtract(a,b){
   return (a - b);
-}function subtract(a,b){
-  return (a - b);
-}function subtract(a,b){
-  return (a - b);
+}
+function multiply(a,b){
+  return (a * b);
+}
+function divide(a,b){
+  return (a / b);
 }
